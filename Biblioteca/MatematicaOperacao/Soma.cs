fa03b6237d7f236c8b28e62440c1ab7d99a29aff
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-03 13:57:14 -0300

    2. Nuget - Criando o projeto.

---
+++ Biblioteca/MatematicaOperacao/Soma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatematicaOperacao {
+    public class opSoma {
+        public static double Somar(double x1, double x2) {
+            return x1 + x2;
+        }
+    }
+}
