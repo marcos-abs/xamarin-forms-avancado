f369e55526880755beb390c1ee6a371979224f40
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-03 18:32:18 -0300

    4. Nuget - Publicando o pacote.

---
+++ Biblioteca/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            double resultado = MatematicaOperacao.opSoma.Somar(1, 2);
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
