6aaa73fbf69324c59d99ef56aa244534cfb07b6c
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-16 16:45:23 -0300

    15. ListView - Personalizando layout do grupo.

--- App2/App2/App2/MainPage.xaml.cs
+++ App2/App2/App2/MainPage.xaml.cs
@@ -15,14 +15,14 @@ namespace App2 {
 
         private List<Grupo> GetFuncionarios () {
             return new List<Grupo> { 
-                new Grupo("Presidente", "CEO") {
+                new Grupo("Presidente", "CEO", "Presidente da Empresa") {
                     new Pessoa { Nome = "José" }
                 },
-                new Grupo("Diretores", "Dir.") {
+                new Grupo("Diretores", "Dir.", "Diretor da Empresa") {
                     new Pessoa { Nome = "João" },
                     new Pessoa { Nome = "Maria" }
                 },
-                new Grupo("Gerentes", "Ger.") {
+                new Grupo("Gerentes", "Ger.", "Gerente da Empresa") {
                     new Pessoa { Nome = "Felipe" },
                     new Pessoa { Nome = "Judas" }
                 }
@@ -32,10 +32,12 @@ namespace App2 {
         public class Grupo : List<Pessoa> {
             public string Titulo { get; set; }
             public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
 
-            public Grupo (string titulo, string tituloCurto) {
+            public Grupo (string titulo, string tituloCurto, string descricao) {
                 this.Titulo = titulo;
                 this.TituloCurto = tituloCurto;
+                this.Descricao = descricao;
             }
         }
 
