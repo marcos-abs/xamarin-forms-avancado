2a60db357cf9f9b8487f8f06d65d91ede2a20d81
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-16 16:08:57 -0300

    14. ListView - Criando grupo simples.

--- App2/App2/App2/MainPage.xaml.cs
+++ App2/App2/App2/MainPage.xaml.cs
@@ -8,21 +8,36 @@ using Xamarin.Forms;
 namespace App2 {
     public partial class MainPage : ContentPage {
         public MainPage() {
-            InitializeComponent ();
+            InitializeComponent();
 
             ListaFuncionarios.ItemsSource = GetFuncionarios();
         }
 
-        private List<Pessoa> GetFuncionarios () {
-            return new List<Pessoa> {
-                new Pessoa { Nome = "José" },
-                new Pessoa { Nome = "João" },
-                new Pessoa { Nome = "Maria" },
-                new Pessoa { Nome = "Felipe" },
-                new Pessoa { Nome = "Judas" }
+        private List<Grupo> GetFuncionarios () {
+            return new List<Grupo> { 
+                new Grupo("Presidente", "CEO") {
+                    new Pessoa { Nome = "José" }
+                },
+                new Grupo("Diretores", "Dir.") {
+                    new Pessoa { Nome = "João" },
+                    new Pessoa { Nome = "Maria" }
+                },
+                new Grupo("Gerentes", "Ger.") {
+                    new Pessoa { Nome = "Felipe" },
+                    new Pessoa { Nome = "Judas" }
+                }
             };
         }
 
+        public class Grupo : List<Pessoa> {
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+
+            public Grupo (string titulo, string tituloCurto) {
+                this.Titulo = titulo;
+                this.TituloCurto = tituloCurto;
+            }
+        }
 
         public class Pessoa {
             public string Nome { get; set; }
