ecd280a611247696bc4655306191883d7735d160
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-16 14:17:03 -0300

    13. ListView - Criação da listagem.

---
+++ App2/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2 {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent ();
+
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+        }
+
+        private List<Pessoa> GetFuncionarios () {
+            return new List<Pessoa> {
+                new Pessoa { Nome = "José" },
+                new Pessoa { Nome = "João" },
+                new Pessoa { Nome = "Maria" },
+                new Pessoa { Nome = "Felipe" },
+                new Pessoa { Nome = "Judas" }
+            };
+        }
+
+
+        public class Pessoa {
+            public string Nome { get; set; }
+        }
+    }
+}
