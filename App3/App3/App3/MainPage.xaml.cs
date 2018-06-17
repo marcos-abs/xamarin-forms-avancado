35a90d3d00d15f086b7d37beb563deba6b8c6f11
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-06-16 21:19:11 -0300

    20. Codificando os controles.

---
+++ App3/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+#if __ANDROID__
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+#endif
+
+#if __IOS__
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+#endif
+
+namespace App3 {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+
+#if __ANDROID__
+            TextView labelDroid = new TextView(Forms.Context) { Text = "Eu sou Android Nativo !" };
+            Container.Children.Add(labelDroid);
+#endif
+
+#if __IOS__
+            UILabel labelIOS = new UILabel() { Text = "Eu sou iOS Nativo!" };
+            Container.Children.Add(labelIOS);
+#endif
+        }
+    }
+}
